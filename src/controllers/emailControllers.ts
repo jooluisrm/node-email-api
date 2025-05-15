990eeefedd36baff4c2824cc6eca8be6d4a37f4a
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-15 10:05:29 -0300

    Enviando e-mails com Node 2

--- src/controllers/emailControllers.ts
+++ src/controllers/emailControllers.ts
@@ -1,17 +1,30 @@
 import { Request, Response } from 'express';
 import nodemailer from 'nodemailer';
 
-export const contato = (req: Request, res: Response) => {
+export const contato = async (req: Request, res: Response) => {
+
+
+
     let transport = nodemailer.createTransport({
         host: "sandbox.smtp.mailtrap.io",
         port: 2525,
         auth: {
             user: "7ac7f622ba19c8",
-            pass: "****7c8c"
+            pass: "b6336003b57c8c"
         }
     });
 
+    let message = {
+        from: 'nao-responda@b7web.com.br',
+        to: 'teste@gmail.com',
+        repleyTo: req.body.from,
+        subject: req.body.subject,
+        html: req.body.email,
+        text: req.body.email
+    }
 
+    let info = await transport.sendMail(message);
+    console.log(info);
 
-    res.json({ pong: true });
+    res.json({ success: true });
 }
